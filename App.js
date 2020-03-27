e395ffb6ff4d9ae8a82c34afcd375b19bfe71ed3
Chris Eckersley <c.eckersley@gmail.com>
2020-03-27 13:52:44 -0700

    added background color

--- App.js
+++ App.js
@@ -4,9 +4,15 @@ import { Input, Card, ListItem } from 'react-native-elements';
 
 import Fpce from './components/FpceCalc'
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f0f0f0'
+  }
+});
+
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Fpce />
     </View>
   );
